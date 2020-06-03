38a0982a38901f7058289078f1fd1941a6a39ef6
mpetrovic <mpetrovic@guneva.net>
2020-06-03 19:42:58 -0400

    Will this fix it?

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@ include_once "vendor/autoload.php";
 
 spl_autoload_register(function ($class) {
     if ($class != 'default') {
-        include_once "src/$class.php";
+        include_once "src\\$class.php";
     }
 });
 ?>
